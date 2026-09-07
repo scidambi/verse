15da86e73c096ca875f9828e5340e02e03685fa9
Sid Cidambi <sid.cidambi@gmail.com>
2026-09-07 14:32:53 -0700

    pset 1: deep thought

---
+++ cs50p/pset1/deep/deep.py
@@ -0,0 +1,7 @@
+answer = input ("What's the answer to 'The Great Question of Life'?")
+answer = answer.strip().title()
+
+if answer == "42" or answer == "Forty-Two" or answer == "Forty-two" or answer == "Forty Two":
+    print("Yes")
+else: print("no")
+
